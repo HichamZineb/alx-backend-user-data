a93487b987b9ad88e28e36c0aab89bcce5467e94
HichamZineb <hichamelamine@hotmail.com>
2024-02-08 09:52:19 +0000

    update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -19,10 +19,11 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        super().__init__(self.FORMAT)
+        super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """Format the specified log record."""
         message = super().format(record)
         for field in self.fields:
             message = re.sub(
