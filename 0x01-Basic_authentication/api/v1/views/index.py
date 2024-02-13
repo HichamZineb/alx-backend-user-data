340fea559a1fff12aa2866fb8937e2699ef51b11
HichamZineb <hichamelamine@hotmail.com>
2024-02-13 15:52:59 +0000

    update index.py

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 """ Module of Index views
 """
-from flask import jsonify, abort, Blueprint
+from flask import jsonify, abort
 from api.v1.views import app_views
-
-app_views = Blueprint('index', __name__)
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
@@ -28,9 +26,19 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/api/v1/unauthorized', methods=['GET'])
-def unauthorized():
-    """
-    Endpoint to simulate an unauthorized request.
+@app_views.route('/unauthorized/', strict_slashes=False)
+def unauthorized() -> None:
+    """GET /api/v1/unauthorized
+    Return:
+      - Unauthorized error.
     """
     abort(401)
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> None:
+    """GET /api/v1/forbidden
+    Return:
+      - Forbidden error.
+    """
+    abort(403)
